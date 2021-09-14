417bd17b0a4dc56a31af188d53f71bb02ec49908
hiro-minami <hiro.0907@icloud.com>
2021-09-14 18:28:53 +0900

    バグ対応

--- src/App.tsx
+++ src/App.tsx
@@ -4,203 +4,105 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
-  // state
-  const [formula, setFormula] = useState("");
-  const [result, setResult] = useState("0");
+    // state
+    const [formula, setFormula] = useState("");
+    const [result, setResult] = useState("0");
 
-  // event
-  const restart = (e: any) => {
-    e.preventDefault();
-    setFormula("");
-    setResult("0");
-  };
-  const changeCord = (e: any) => {
-    e.preventDefault();
-    setResult(OperatorHelper.optimizeNumber((+result * -1).toFixed(5)));
-  };
-  const addFormulaForOperator = (e: any) => {
-    e.preventDefault();
-    if (InputHelper.canInputOperator(formula, e.target.value) === true) {
-      setFormula(formula + e.target.value);
-    }
-  };
-  const addFormulaForOperand = (e: any) => {
-    e.preventDefault();
-    setFormula(formula + e.target.value);
-  };
-  const calculate = (e: any) => {
-    e.preventDefault();
-    if (
-      formula !== "" &&
-      InputHelper.canCalculate(formula[formula.length - 1]) === true
-    )
-      setResult(expressionParser(formula));
-    setFormula("");
-  };
+    // event
+    const restart = (e: any) => {
+        e.preventDefault();
+        setFormula("");
+        setResult("0");
+    };
+    const changeCord = (e: any) => {
+        e.preventDefault();
+        setResult(OperatorHelper.optimizeNumber((+result * -1).toFixed(5)));
+    };
+    const addFormulaForOperator = (e: any) => {
+        e.preventDefault();
+        if (InputHelper.canInputOperator(formula, e.target.value) === true) {
+            setFormula(formula + e.target.value);
+        }
+    };
+    const addFormulaForOperand = (e: any) => {
+        e.preventDefault();
+        setFormula(formula + e.target.value);
+    };
+    const calculate = (e: any) => {
+        e.preventDefault();
+        if (formula !== "" && InputHelper.canCalculate(formula[formula.length - 1]) === true) setResult(expressionParser(formula));
+        setFormula("");
+    };
 
-  return (
-    <div className="container d-flex justify-content-center">
-      <div className="col-8 col-sm-5 col-md-4 col-lg-3 col-xl-2 m-5 bg-black rounded transparency">
-        <div className="display-parent display">
-          <div className="text-end fs-3 font-light m-1">{formula}</div>
-          <div className="text-end font-lg display-answer font-light m-1">
-            {result}
-          </div>
+    return (
+        <div className="container d-flex justify-content-center">
+            <div className="col-8 col-sm-5 col-md-4 col-lg-3 col-xl-2 m-5 bg-black rounded transparency">
+                <div className="display-parent display">
+                    <div className="text-end fs-3 font-light m-1">{formula}</div>
+                    <div className="text-end font-lg display-answer font-light m-1">{result}</div>
+                </div>
+                <div className="buttons">
+                    <button type="button" className="bg-command btn-lg col-3 rounded transparency-button font-light" onClick={restart}>
+                        AC
+                    </button>
+                    <button type="button" className="bg-command btn-lg col-3 rounded transparency-button font-light" onClick={changeCord}>
+                        +/-
+                    </button>
+                    <button type="button" className="bg-command btn-lg col-3 rounded transparency-button font-light" value="%" onClick={addFormulaForOperator}>
+                        %
+                    </button>
+                    <button type="button" className="bg-operator btn-lg col-3 rounded font-light" value="÷" onClick={addFormulaForOperator}>
+                        ÷
+                    </button>
+                    <button type="button" className="bg-secondary btn-lg col-3 rounded font-light" value="7" onClick={addFormulaForOperand}>
+                        7
+                    </button>
+                    <button type="button" className="bg-secondary btn-lg col-3 rounded font-light" value="8" onClick={addFormulaForOperand}>
+                        8
+                    </button>
+                    <button type="button" className="bg-secondary btn-lg col-3 rounded font-light" value="9" onClick={addFormulaForOperand}>
+                        9
+                    </button>
+                    <button type="button" className="bg-operator btn-lg col-3 rounded font-light" value="×" onClick={addFormulaForOperator}>
+                        ×
+                    </button>
+                    <button type="button" className="bg-secondary btn-lg col-3 rounded font-light" value="4" onClick={addFormulaForOperand}>
+                        4
+                    </button>
+                    <button type="button" className="bg-secondary btn-lg col-3 rounded font-light" value="5" onClick={addFormulaForOperand}>
+                        5
+                    </button>
+                    <button type="button" className="bg-secondary btn-lg col-3 rounded font-light" value="6" onClick={addFormulaForOperand}>
+                        6
+                    </button>
+                    <button type="button" className="bg-operator btn-lg col-3 rounded font-light" value="-" onClick={addFormulaForOperator}>
+                        -
+                    </button>
+                    <button type="button" className="bg-secondary btn-lg col-3 rounded font-light" value="1" onClick={addFormulaForOperand}>
+                        1
+                    </button>
+                    <button type="button" className="bg-secondary btn-lg col-3 rounded font-light" value="2" onClick={addFormulaForOperand}>
+                        2
+                    </button>
+                    <button type="button" className="bg-secondary btn-lg col-3 rounded font-light" value="3" onClick={addFormulaForOperand}>
+                        3
+                    </button>
+                    <button type="button" className="bg-operator btn-lg col-3 rounded font-light" value="+" onClick={addFormulaForOperator}>
+                        +
+                    </button>
+                    <button type="button" className="bg-secondary btn-lg col-6 rounded font-light" value="0" onClick={addFormulaForOperand}>
+                        0
+                    </button>
+                    <button type="button" className="bg-secondary btn-lg col-3 rounded font-light" value="." onClick={addFormulaForOperator}>
+                        .
+                    </button>
+                    <button type="button" className="bg-operator btn-lg col-3 rounded font-light" onClick={calculate}>
+                        =
+                    </button>
+                </div>
+            </div>
         </div>
-        <div className="buttons">
-          <button
-            type="button"
-            className="bg-command btn-lg col-3 rounded transparency-button font-light"
-            onClick={restart}
-          >
-            AC
-          </button>
-          <button
-            type="button"
-            className="bg-command btn-lg col-3 rounded transparency-button font-light"
-            onClick={changeCord}
-          >
-            +/-
-          </button>
-          <button
-            type="button"
-            className="bg-command btn-lg col-3 rounded transparency-button font-light"
-            value="%"
-            onClick={addFormulaForOperator}
-          >
-            %
-          </button>
-          <button
-            type="button"
-            className="bg-operator btn-lg col-3 rounded font-light"
-            value="÷"
-            onClick={addFormulaForOperator}
-          >
-            ÷
-          </button>
-          <button
-            type="button"
-            className="bg-secondary btn-lg col-3 rounded font-light"
-            value="7"
-            onClick={addFormulaForOperand}
-          >
-            7
-          </button>
-          <button
-            type="button"
-            className="bg-secondary btn-lg col-3 rounded font-light"
-            value="8"
-            onClick={addFormulaForOperand}
-          >
-            8
-          </button>
-          <button
-            type="button"
-            className="bg-secondary btn-lg col-3 rounded font-light"
-            value="9"
-            onClick={addFormulaForOperand}
-          >
-            9
-          </button>
-          <button
-            type="button"
-            className="bg-operator btn-lg col-3 rounded font-light"
-            value="×"
-            onClick={addFormulaForOperator}
-          >
-            ×
-          </button>
-          <button
-            type="button"
-            className="bg-secondary btn-lg col-3 rounded font-light"
-            value="4"
-            onClick={addFormulaForOperand}
-          >
-            4
-          </button>
-          <button
-            type="button"
-            className="bg-secondary btn-lg col-3 rounded font-light"
-            value="5"
-            onClick={addFormulaForOperand}
-          >
-            5
-          </button>
-          <button
-            type="button"
-            className="bg-secondary btn-lg col-3 rounded font-light"
-            value="6"
-            onClick={addFormulaForOperand}
-          >
-            6
-          </button>
-          <button
-            type="button"
-            className="bg-operator btn-lg col-3 rounded font-light"
-            value="-"
-            onClick={addFormulaForOperator}
-          >
-            -
-          </button>
-          <button
-            type="button"
-            className="bg-secondary btn-lg col-3 rounded font-light"
-            value="1"
-            onClick={addFormulaForOperand}
-          >
-            1
-          </button>
-          <button
-            type="button"
-            className="bg-secondary btn-lg col-3 rounded font-light"
-            value="2"
-            onClick={addFormulaForOperand}
-          >
-            2
-          </button>
-          <button
-            type="button"
-            className="bg-secondary btn-lg col-3 rounded font-light"
-            value="3"
-            onClick={addFormulaForOperand}
-          >
-            3
-          </button>
-          <button
-            type="button"
-            className="bg-operator btn-lg col-3 rounded font-light"
-            value="+"
-            onClick={addFormulaForOperator}
-          >
-            +
-          </button>
-          <button
-            type="button"
-            className="bg-secondary btn-lg col-6 rounded font-light"
-            value="0"
-            onClick={addFormulaForOperand}
-          >
-            0
-          </button>
-          <button
-            type="button"
-            className="bg-secondary btn-lg col-3 rounded font-light"
-            value="."
-            onClick={addFormulaForOperator}
-          >
-            .
-          </button>
-          <button
-            type="button"
-            className="bg-operator btn-lg col-3 rounded font-light"
-            onClick={calculate}
-          >
-            =
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default App;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -9,113 +9,86 @@ priority.set("noOperator", -1);
 
 // 入力用のヘルパークラス
 export class InputHelper {
-  public static canInputOperator(str: string, char: string): boolean {
-    if (char === "." && this.hasPeriod(str) === true) return false;
-    return this.isOperand(str[str.length - 1]) === true;
-  }
-  public static canCalculate(str: string): boolean {
-    return this.isOperand(str[str.length - 1]) === true;
-  }
-  private static hasPeriod(str: string): boolean {
-    const operand = str.split(/[+|\-|×|÷|%]/g);
-    return operand[operand.length - 1].includes(".");
-  }
-  private static isOperand(char: string): boolean {
-    if (!char) return false;
-    return priority.get(char) === undefined;
-  }
+    public static canInputOperator(str: string, char: string): boolean {
+        if (char === "." && this.hasPeriod(str) === true) return false;
+        return this.isOperand(str[str.length - 1]) === true;
+    }
+    public static canCalculate(str: string): boolean {
+        return this.isOperand(str[str.length - 1]) === true;
+    }
+    private static hasPeriod(str: string): boolean {
+        const operand = str.split(/[+|\-|×|÷|%]/g);
+        return operand[operand.length - 1].includes(".");
+    }
+    private static isOperand(char: string): boolean {
+        if (!char) return false;
+        return priority.get(char) === undefined && char !== ".";
+    }
 }
 
 // 計算用のヘルパークラス
 export class OperatorHelper {
-  private static trimZero(str: string): string {
-    return this.trimZeroHelper(str, str.length - 1);
-  }
-  private static trimZeroHelper(str: string, length: number): string {
-    if (
-      length <= 0 ||
-      (str.charAt(length) !== "0" && str.charAt(length) !== ".")
-    )
-      return str;
-    return this.trimZeroHelper(str.substring(0, length), length - 1);
-  }
-  public static optimizeNumber(str: string): string {
-    return str.includes(".") ? this.trimZero(str) : str;
-  }
-  public static operate(
-    operandStack: number[],
-    operatorStack: string[],
-    operate: string = "noOperator"
-  ): string {
-    if (operatorStack.length === 0)
-      return this.optimizeNumber(operandStack[0].toFixed(5));
-    let ans = 0;
-    while (
-      operatorStack.length !== 0 &&
-      priority.get(operatorStack[operatorStack.length - 1])! >=
-        priority.get(operate)!
-    ) {
-      let num1 = operandStack.pop();
-      let num2 = operandStack.pop();
-      let operator = operatorStack.pop();
-      if (operator === "+") ans = num2! + num1!;
-      else if (operator === "-") ans = num2! - num1!;
-      else if (operator === "×") ans = num2! * num1!;
-      else if (operator === "÷") ans = num2! / num1!;
-      else if (operator === "%") ans = num2! % num1!;
-      // 計算結果をスタックに追加
-      operandStack.push(ans);
+    private static trimZero(str: string): string {
+        return this.trimZeroHelper(str, str.length - 1);
     }
-    return Number.isInteger(ans)
-      ? ans.toString()
-      : this.trimZero(ans.toFixed(5));
-  }
+    private static trimZeroHelper(str: string, length: number): string {
+        if (length <= 0 || (str.charAt(length) !== "0" && str.charAt(length) !== ".")) return str;
+        return this.trimZeroHelper(str.substring(0, length), length - 1);
+    }
+    public static optimizeNumber(str: string): string {
+        return str.includes(".") ? this.trimZero(str) : str;
+    }
+    public static operate(operandStack: number[], operatorStack: string[], operate: string = "noOperator"): string {
+        if (operatorStack.length === 0) return this.optimizeNumber(operandStack[0].toFixed(5));
+        let ans = 0;
+        while (operatorStack.length !== 0 && priority.get(operatorStack[operatorStack.length - 1])! >= priority.get(operate)!) {
+            let num1 = operandStack.pop();
+            let num2 = operandStack.pop();
+            let operator = operatorStack.pop();
+            if (operator === "+") ans = num2! + num1!;
+            else if (operator === "-") ans = num2! - num1!;
+            else if (operator === "×") ans = num2! * num1!;
+            else if (operator === "÷") ans = num2! / num1!;
+            else if (operator === "%") ans = num2! % num1!;
+            // 計算結果をスタックに追加
+            operandStack.push(ans);
+        }
+        return Number.isInteger(ans) ? ans.toString() : this.trimZero(ans.toFixed(5));
+    }
 }
 
 export const expressionParser = (expression: string): string => {
-  // stack
-  let operandStack: number[] = [];
-  let operatorStack: string[] = [];
-  let i = 0;
-  while (i < expression.length) {
-    // オペランドの場合
-    if (priority.get(expression.charAt(i)) === undefined) {
-      let ope = "";
-      let num = 0;
-      while (
-        priority.get(expression.charAt(i + num)) === undefined &&
-        i + num < expression.length
-      ) {
-        ope += expression.charAt(i + num);
-        num++;
-      }
-      operandStack.push(parseFloat(ope));
-      i += num;
-    } else {
-      // 演算子の場合
-      if (operatorStack.length === 0) {
-        // 演算子スタックが空の場合は、それを演算子スタックにプッシュ
-        operatorStack.push(expression.charAt(i));
-      } else if (
-        priority.get(operatorStack[operatorStack.length - 1])! <
-        priority.get(expression.charAt(i))!
-      ) {
-        // 演算子スタックが空ではなく、その文字の優先順位が演算子スタックの頂上の優先順位よりも大きい場合、その文字を演算子スタックにプッシュ
-        operatorStack.push(expression.charAt(i));
-      } else {
-        // 計算する
-        OperatorHelper.operate(
-          operandStack,
-          operatorStack,
-          expression.charAt(i)
-        );
-        // 計算結果をoperandStackに入れる
-        operatorStack.push(expression.charAt(i));
-      }
-      i++;
+    // stack
+    let operandStack: number[] = [];
+    let operatorStack: string[] = [];
+    let i = 0;
+    while (i < expression.length) {
+        // オペランドの場合
+        if (priority.get(expression.charAt(i)) === undefined) {
+            let ope = "";
+            let num = 0;
+            while (priority.get(expression.charAt(i + num)) === undefined && i + num < expression.length) {
+                ope += expression.charAt(i + num);
+                num++;
+            }
+            operandStack.push(parseFloat(ope));
+            i += num;
+        } else {
+            // 演算子の場合
+            if (operatorStack.length === 0) {
+                // 演算子スタックが空の場合は、それを演算子スタックにプッシュ
+                operatorStack.push(expression.charAt(i));
+            } else if (priority.get(operatorStack[operatorStack.length - 1])! < priority.get(expression.charAt(i))!) {
+                // 演算子スタックが空ではなく、その文字の優先順位が演算子スタックの頂上の優先順位よりも大きい場合、その文字を演算子スタックにプッシュ
+                operatorStack.push(expression.charAt(i));
+            } else {
+                // 計算する
+                OperatorHelper.operate(operandStack, operatorStack, expression.charAt(i));
+                // 計算結果をoperandStackに入れる
+                operatorStack.push(expression.charAt(i));
+            }
+            i++;
+        }
     }
-  }
-  return operatorStack[0] !== null
-    ? OperatorHelper.operate(operandStack, operatorStack)
-    : OperatorHelper.optimizeNumber(operandStack[0].toFixed(5));
+    return operatorStack[0] !== null ? OperatorHelper.operate(operandStack, operatorStack) : OperatorHelper.optimizeNumber(operandStack[0].toFixed(5));
 };
